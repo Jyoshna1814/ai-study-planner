5cbe1509eaea6dace867ec283a3e9748ea623607
Jyoshnarani <sasmaljyoshna1814@gmail.com>
2026-03-11 19:32:10 +0530

    AI Study Planner

---
+++ public/script.js
@@ -0,0 +1,338 @@
+let currentUser = null
+
+function signup(){
+
+let username = document.getElementById("username").value
+let password = document.getElementById("password").value
+
+fetch("/signup",{
+
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({username,password})
+
+})
+.then(res=>res.json())
+.then(data=>{
+alert(data.message)
+})
+
+}
+
+
+function login(){
+
+let username = document.getElementById("username").value
+let password = document.getElementById("password").value
+
+fetch("/login",{
+
+method:"POST",
+headers:{"Content-Type":"application/json"},
+body:JSON.stringify({username,password})
+
+})
+.then(res=>res.json())
+.then(data=>{
+
+if(data.message=="Login successful"){
+
+currentUser=data.user
+
+localStorage.setItem("user", JSON.stringify(currentUser))
+
+loadSubjects()
+
+document.getElementById("authBox").style.display="none"
+document.getElementById("logoutBtn").style.display="block"
+
+}
+
+else{
+
+alert("Login failed")
+
+}
+
+})
+
+}
+function logout(){
+
+currentUser=null
+
+localStorage.removeItem("user")
+
+document.getElementById("authBox").style.display="block"
+document.getElementById("logoutBtn").style.display="none"
+
+document.getElementById("subjectList").innerHTML=""
+
+}
+function addSubject(){
+
+let subject=document.getElementById("subjectInput").value
+
+fetch("/add-subject",{
+
+method:"POST",
+
+headers:{"Content-Type":"application/json"},
+
+body:JSON.stringify({
+
+user:currentUser.username,
+subject:subject
+
+})
+
+})
+
+.then(res=>res.json())
+
+.then(data=>{
+
+loadSubjects()
+
+})
+
+}
+function loadSubjects(){
+
+fetch("/subjects/"+currentUser.username)
+
+.then(res=>res.json())
+
+.then(data=>{
+
+let list=document.getElementById("subjectList")
+
+list.innerHTML=""
+
+data.forEach(sub=>{
+
+let li=document.createElement("li")
+
+li.innerHTML=
+
+sub.name+
+
+` <button onclick="editSubject('${sub._id}','${sub.name}')">Edit</button>
+
+<button onclick="deleteSubject('${sub._id}')">Delete</button>`
+
+list.appendChild(li)
+
+})
+
+})
+
+}
+function deleteSubject(id){
+
+fetch("/delete-subject/"+id,{
+
+method:"DELETE"
+
+})
+
+.then(()=>{
+
+loadSubjects()
+
+})
+
+}
+function editSubject(id,name){
+
+let newName = prompt("Edit subject",name)
+
+fetch("/edit-subject/"+id,{
+
+method:"PUT",
+
+headers:{"Content-Type":"application/json"},
+
+body:JSON.stringify({name:newName})
+
+})
+
+.then(()=>{
+
+loadSubjects()
+
+})
+
+}
+function generatePlan(){
+
+let examDate=document.getElementById("examDate").value
+let hours=document.getElementById("studyHours").value
+
+let subjects=[]
+
+document.querySelectorAll("#subjectList li").forEach(li=>{
+subjects.push({name:li.firstChild.textContent,difficulty:3,weightage:1})
+})
+
+fetch("/generate-plan",{
+
+method:"POST",
+
+headers:{"Content-Type":"application/json"},
+
+body:JSON.stringify({
+
+subjects:subjects,
+examDate:examDate,
+hours:hours
+
+})
+
+})
+
+.then(res=>res.json())
+
+.then(data=>{
+
+let result=document.getElementById("planResult")
+
+result.innerHTML=""
+
+result.innerHTML += "<h3>Days Left: "+data.daysLeft+"</h3>"
+
+data.plan.forEach(p=>{
+
+result.innerHTML += `
+<div>
+${p.subject} : ${p.recommendedHours} hrs
+</div>
+`
+
+})
+
+showChart(data.plan)
+
+})
+
+}
+function showChart(plan){
+
+let labels=[]
+let data=[]
+
+plan.forEach(p=>{
+
+labels.push(p.subject)
+data.push(p.recommendedHours)
+
+})
+
+const ctx = document.getElementById('progressChart')
+
+new Chart(ctx,{
+
+type:'bar',
+
+data:{
+
+labels:labels,
+
+datasets:[{
+
+label:'Recommended Study Hours',
+
+data:data,
+
+borderWidth:1
+
+}]
+
+},
+
+options:{
+
+responsive:true
+
+}
+
+})
+
+}
+let timer
+let timeLeft = 25 * 60
+
+function updateTimer(){
+
+let minutes = Math.floor(timeLeft / 60)
+let seconds = timeLeft % 60
+
+document.getElementById("timer").textContent =
+minutes.toString().padStart(2,"0") + ":" +
+seconds.toString().padStart(2,"0")
+
+}
+
+function startTimer(){
+
+if(timer) return
+
+timer = setInterval(()=>{
+
+timeLeft--
+
+updateTimer()
+
+if(timeLeft <= 0){
+
+clearInterval(timer)
+timer=null
+
+alert("Pomodoro session complete!")
+
+}
+
+},1000)
+
+}
+
+function stopTimer(){
+
+clearInterval(timer)
+timer=null
+
+}
+
+function resetTimer(){
+
+clearInterval(timer)
+
+timer=null
+
+timeLeft = 25 * 60
+
+updateTimer()
+
+}
+
+updateTimer()
+window.onload = function(){
+
+let savedUser = localStorage.getItem("user")
+
+if(savedUser){
+
+currentUser = JSON.parse(savedUser)
+
+document.getElementById("authBox").style.display="none"
+document.getElementById("logoutBtn").style.display="block"
+
+loadSubjects()
+
+}
+
+}
+if ("serviceWorker" in navigator) {
+navigator.serviceWorker.register("sw.js")
+.then(() => console.log("Service Worker Registered"))
+}
---
+++ server.js
@@ -0,0 +1,178 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.static("public"))
+
+mongoose.connect("mongodb+srv://jyoshna:sasmal1814@cluster0.0fnvv0m.mongodb.net/studyplanner")
+
+.then(()=>{
+
+console.log("MongoDB connected")
+
+})
+
+.catch(err=>{
+
+console.log(err)
+
+})
+
+const UserSchema = new mongoose.Schema({
+username:String,
+password:String
+})
+
+const User = mongoose.model("User", UserSchema)
+
+// SUBJECT SCHEMA
+const SubjectSchema = new mongoose.Schema({
+user:String,
+name:String
+})
+
+const Subject = mongoose.model("Subject",SubjectSchema)
+
+// SIGNUP API
+app.post("/signup", async (req,res)=>{
+
+const {username,password} = req.body
+
+const existingUser = await User.findOne({username})
+
+if(existingUser){
+return res.json({message:"User already exists"})
+}
+
+const newUser = new User({
+username,
+password
+})
+
+await newUser.save()
+
+res.json({message:"Signup successful"})
+
+})
+
+// LOGIN API
+app.post("/login", async (req,res)=>{
+
+const {username,password} = req.body
+
+const user = await User.findOne({username,password})
+
+if(!user){
+return res.json({message:"Invalid login"})
+}
+
+res.json({message:"Login successful",user})
+
+})
+
+// ADD SUBJECT
+app.post("/add-subject", async (req,res)=>{
+
+const {user,subject} = req.body
+
+const newSubject = new Subject({
+
+user:user,
+name:subject
+
+})
+
+await newSubject.save()
+
+res.json({message:"Subject added"})
+
+})
+// GET SUBJECTS
+app.get("/subjects/:user", async (req,res)=>{
+
+const subjects = await Subject.find({user:req.params.user})
+
+res.json(subjects)
+
+})
+// DELETE SUBJECT
+app.delete("/delete-subject/:id", async (req,res)=>{
+
+await Subject.findByIdAndDelete(req.params.id)
+
+res.json({message:"Subject deleted"})
+
+})
+// EDIT SUBJECT
+app.put("/edit-subject/:id", async (req,res)=>{
+
+await Subject.findByIdAndUpdate(
+
+req.params.id,
+{name:req.body.name}
+
+)
+
+res.json({message:"Subject updated"})
+
+})
+// AI STUDY PLAN GENERATOR 
+app.post("/generate-plan",(req,res)=>{
+
+const {subjects, examDate, hours} = req.body
+
+const today = new Date()
+const exam = new Date(examDate)
+
+const daysLeft = Math.ceil((exam - today)/(1000*60*60*24))
+
+if(daysLeft <= 0){
+return res.json({message:"Exam date passed"})
+}
+
+let totalWeight = 0
+
+subjects.forEach(s=>{
+totalWeight += (s.difficulty * s.weightage)
+})
+
+let plan=[]
+
+subjects.forEach(s=>{
+
+let subjectWeight = (s.difficulty * s.weightage) / totalWeight
+
+let totalStudyHours = hours * daysLeft * subjectWeight
+
+plan.push({
+
+subject:s.name,
+difficulty:s.difficulty,
+weightage:s.weightage,
+recommendedHours: Math.round(totalStudyHours)
+
+})
+
+})
+
+res.json({
+
+daysLeft:daysLeft,
+plan:plan
+
+})
+
+})
+
+app.get("/", (req,res)=>{
+res.sendFile(__dirname + "/public/index.html")
+})
+
+const PORT = 3000
+
+app.listen(PORT,()=>{
+console.log("Server running on port " + PORT)
+})
